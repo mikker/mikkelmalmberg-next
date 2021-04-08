a9cd9aada30e7bd3a11a60a19de83b7a8a939896
Mikkel Malmberg <mikkel@brnbw.com>
2021-04-08 09:07:53 +0200

    Add SimpleAnalytics

---
+++ pages/_document.js
@@ -0,0 +1,24 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <script async defer src="https://sa.mikkelmalmberg.com/latest.js"></script>
+          <noscript><img src="https://sa.mikkelmalmberg.com/noscript.gif" alt=""/></noscript>
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
