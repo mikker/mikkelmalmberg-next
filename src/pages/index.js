2e8eb78144166aee79640f41a7eec7b952701051
Mikkel Malmberg <mikkel@brnbw.com>
2022-03-04 11:58:06 +0100

    Update deps, add media

--- src/pages/index.js
+++ src/pages/index.js
@@ -62,6 +62,31 @@ export default function AboutPage() {
 
       <Container>
         <div className="grid grid-cols-1 md:grid-cols-12 gap-8">
+          <h2 className="col-span-2 text-tertiary md:text-right">Media</h2>
+
+          <div className="flex flex-col overflow-auto col-span-10 space-y-3">
+            {media.map((position, i) => (
+              <a
+                href={position.link}
+                className="items-center md:flex md:space-x-4 group"
+                key={i}
+              >
+                <div className="flex-none group-hover:link ">
+                  {position.title}
+                </div>
+                <div className="flex-shrink hidden w-full border-t-2 border-gray-200 md:block dark:border-gray-800"></div>
+                <div className="flex-none text-secondary">{position.role}</div>
+                <div className="flex-none text-tertiary lining-nums tabular-nums">
+                  {position.duration}
+                </div>
+              </a>
+            ))}
+          </div>
+        </div>
+      </Container>
+
+      <Container>
+        <div className="grid grid-cols-1 md:grid-cols-12 gap-8">
           <h2 className="col-span-2 text-tertiary md:text-right">Find me</h2>
 
           <div className="flex flex-col overflow-auto col-span-10 space-y-3">
@@ -87,22 +112,16 @@ export default function AboutPage() {
 
 const work = [
   {
-    title: "Brainbow",
-    role: "Owner",
-    duration: "2007–____",
-    link: "https://brainbow.studio",
-  },
-  {
     title: "Random Character Collective",
     role: "dev",
-    duration: "2021–____",
+    duration: "2021-____",
     link: "https://twitter.com/rndmcharacters",
   },
   {
-    title: "Kortsluttet",
-    role: "Radio Host, P1",
-    duration: "2020-2021",
-    link: "https://www.dr.dk/lyd/p1/kortsluttet",
+    title: "Brainbow",
+    role: "Solo founder",
+    duration: "2007-____",
+    link: "https://brainbow.studio",
   },
   {
     title: "Elastic",
@@ -135,24 +154,39 @@ const work = [
     link: "https://firmafon.dk",
   },
   {
+    title: "Wemind",
+    role: "Student Developer",
+    duration: "2008-2010",
+    link: "https://wemind.dk",
+  },
+];
+
+const media = [
+  {
+    title: "Kortsluttet",
+    role: "Radio Host, P1",
+    duration: "2020-2021",
+    link: "https://www.dr.dk/lyd/p1/kortsluttet",
+  },
+  {
     title: "Fup i Farvandet",
     role: "Podcast host",
     duration: "2011-2018",
     link: "https://fupifarvandet.dk",
   },
   {
-    title: "Stand-up comedian",
-    role: "Jokes, primarily",
-    duration: "2009–2018",
-    link: "https://www.youtube.com/watch?v=TgzjUb-2blI",
+    title: "Mikkel og et Menneske",
+    role: "Podcast host",
+    duration: "2012-2012",
+    link: "http://moem.dk",
   },
   {
-    title: "Wemind",
-    role: "Student Developer",
-    duration: "2008–2010",
-    link: "https://wemind.dk",
+    title: "Stand-up comedian",
+    role: "Jokes, primarily",
+    duration: "2009-2018",
+    link: "https://www.youtube.com/watch?v=TgzjUb-2blI",
   },
-];
+]
 
 const elsewhere = [
   { title: "twitter.com/mikker", link: "https://twitter.com/mikker" },
